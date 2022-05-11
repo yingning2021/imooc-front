1205746e1a67460c69abf525b4b3ee49abc134d9
yingning2021 <earthbrithday@163.com>
2022-05-11 16:07:34 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img
+      class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none"
+      src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+      alt=""
+    />
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        “这是使用 tailwindcss
+        来实现的一个基础的卡片区域，在实现该卡片区域的过程中，我们从来没有离开过
+        HTML 区域.”
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">LGD_Sunday</div>
+        <div class="text-slate-500">中前台</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
